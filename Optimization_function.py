9d3694c83403d8c2d2d5e24c8ca0381196636e84
Qinyi <59978382+liqinyi97@users.noreply.github.com>
2020-07-31 19:39:01 -0400

    Add files via upload

--- Optimization_function.py
+++ Optimization_function.py
@@ -11,7 +11,7 @@ from scipy.stats import chi2
 
 
 
-def Markovitz_optimization(mean, cov, t):
+def Markovitz_opt(mean, cov, t):
     a = 2/t
     N = len(mean)
     
@@ -27,13 +27,31 @@ def Markovitz_optimization(mean, cov, t):
     return w
 
 
+def Markovitz_opt_risktarget(mean, cov, risk_level):
+    N = len(mean) 
+    X = np.linalg.cholesky(cov)
+    
+    c = matrix(-mean)
+    A = matrix(np.array([[1.0 for i in range(N)]]), tc='d')
+    b = matrix(np.array([1.0]), tc='d')
+    
+    Gl = matrix(np.diag([-1.0 for i in range(N)]), tc='d')
+    hl = matrix(np.array([0.0 for i in range(N)]), tc='d')
+    Gq = [matrix(np.vstack([np.array([0.0 for i in range(N)]), (-1)*X.T]))]
+    hq = [matrix(np.array([risk_level] + [0.0 for i in range(N)]))]
+    
+    sol = solvers.socp(c, Gl = Gl, hl = hl, Gq = Gq, hq = hq, A = A, b = b)
+    w = sol['x']
+    #CE_0 = -sol['primal objective']
+    return w
 
 
-def Robust_optimization(mean, cov, sigma, t, eta):
+
+def Robust_opt(mean, cov, sigma, t, eta):
     N = len(mean)
     a = 2/t
     kappa = np.sqrt(chi2.ppf(eta, df = N))
-    L = np.linalg.cholesky(sigma)
+    Y = np.linalg.cholesky(sigma)
     
     P_0 = np.hstack([a*cov, np.array([[0.0] for i in range(N)])])
     P_0 = np.vstack([P_0, np.array([0.0 for i in range(N+1)])])
@@ -47,7 +65,7 @@ def Robust_optimization(mean, cov, sigma, t, eta):
     
     I = matrix(0.0, (N+1,N+1))
     I[::N+2] = 1.0
-    G_1 = np.hstack([(-1)*L, np.array([[0.0] for i in range(N)])])
+    G_1 = np.hstack([(-1)*Y.T, np.array([[0.0] for i in range(N)])])
     G_1 = np.vstack([np.array([0.0 for i in range(N)] + [-1.0]), G_1])
     G = matrix([-I, matrix(G_1)])
     h = matrix((N+1)*[0.0] + (N+1)*[0.0])
@@ -59,3 +77,31 @@ def Robust_optimization(mean, cov, sigma, t, eta):
     #CE_0 = np.dot(mean,w)[0] - 0.5*a*np.dot(np.dot(w.T, cov), w)[0][0]
     
     return w
+
+
+def Robust_opt_risktarget(mean, cov, sigma, eta, risk_level):
+    N = len(mean)
+    kappa = np.sqrt(chi2.ppf(eta, df = N))
+    X = np.linalg.cholesky(cov)
+    Y = np.linalg.cholesky(sigma)
+    
+    c = matrix(np.append(-mean, [kappa]))
+    A = matrix(np.array([[1.0 for i in range(N)] + [0.0]]), tc='d')
+    b = matrix(np.array([1.0]), tc='d')
+    
+    Gl = matrix(np.diag([-1.0 for i in range(N+1)]), tc='d')
+    hl = matrix(np.array([0.0 for i in range(N+1)]), tc='d')
+    G_0 = np.hstack([(-1)*X.T, np.array([[0.0] for i in range(N)])])
+    G_0 = np.vstack([np.array([0.0 for i in range(N+1)]), G_0])
+    Gq = [matrix(G_0)]
+    hq = [matrix(np.array([risk_level] + [0.0 for i in range(N)]))]
+    G_1 = np.hstack([(-1)*Y.T, np.array([[0.0] for i in range(N)])])
+    G_1 = np.vstack([np.array([0.0 for i in range(N)] + [-1.0]), G_1])
+    Gq += [matrix(G_1)]
+    hq += [matrix(np.array([0.0 for i in range(N+1)]))]
+    
+    sol = solvers.socp(c, Gl = Gl, hl = hl, Gq = Gq, hq = hq, A = A, b = b)
+    w = sol['x'][:-1]
+    
+    return w
+    
